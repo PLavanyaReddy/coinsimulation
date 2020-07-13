fb2b2db76a401c11e81c4e48c7a6739b094bcfb6
unknown <lavanyareddy19994@gmail.com>
2020-07-13 23:23:06 +0530

    coin flipped till either wins

--- coin.sh
+++ coin.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-read -p "Enter number of times the coin should be flipped: " num
 heads=0
 tails=0
-for (( i=1;i<=$num;i++ ))
+flag=0
+while [ $flag -eq 0 ]
 do
 	flip=$(( RANDOM%2 ))
 	if [ $flip -eq 1 ]
@@ -10,6 +10,10 @@ do
 		heads=$(( $heads+1 ))
 	else
 		tails=$(( $tails+1 ))
+	fi
+	if [ $heads -eq 21 ] || [ $tails -eq 21 ]
+	then
+		flag=1
 	fi
 done
 echo "Heads appered "$heads" times"
