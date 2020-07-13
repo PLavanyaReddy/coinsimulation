f3a35884fc2c0d15d6569a465002d8cabbbe46af
unknown <lavanyareddy19994@gmail.com>
2020-07-13 23:36:58 +0530

    coin modified

--- coin.sh
+++ coin.sh
@@ -14,8 +14,28 @@ do
 	if [ $heads -eq 21 ] || [ $tails -eq 21 ]
 	then
 		flag=1
+	elif [ $heads -eq 21 ] && [ $tails -eq 21 ]
+	then
+		flag=2
 	fi
 done
+if [ $flag -eq 2 ]
+then
+	while [ $flag -eq 2 ]
+	do
+		flip=$(( RANDOM%2 ))
+        if [ $flip -eq 1 ]
+        then
+                heads=$(( $heads+1 ))
+        else
+                tails=$(( $tails+1 ))
+        fi
+	if [ $(( $heads-$tails )) -eq 2 ] || [ $(( $tails-$heads )) -eq 2 ]
+	then
+		flag=0
+	fi
+	done
+fi
 echo "Heads appered "$heads" times"
 echo "Tails appeared "$tails" times"
 
